7a9e67146eab0cca8b5c55f13f5bce8bea48945c
Yogesh Sahu <ysahu1590@gmail.com>
2026-07-30 23:26:30 +0530

    Set up routing with Login and Register pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.tsx';
+import LoginPage from '../components/LoginPage.tsx';
+import RegisterUser from "../components/RegisterUser.tsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterUser />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
