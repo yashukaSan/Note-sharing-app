5f57d1a5eb04ef1fb91fc30aa3e502727ac05610
yashukaSan <ysahu1590@gmail.com>
2026-08-03 23:01:59 +0530

    Style the Features section

--- client/components/MainPage.tsx
+++ client/components/MainPage.tsx
@@ -6,6 +6,11 @@ import { motion, useScroll, useMotionValueEvent } from "motion/react";
 import { useState } from "react";
 import Typewriter from 'typewriter-effect';
 
+type FeatureBox ={
+  h5: string, 
+  p: string
+};
+
 export default function MainPage():JSX.Element {
   const { scrollY } = useScroll();
   const [hidden, setHidden] = useState(false);
@@ -18,6 +23,33 @@ export default function MainPage():JSX.Element {
       setHidden(false);
     }
   });
+
+  const featureBox: FeatureBox[] = [
+    {
+      h5: "Use it everywhere",
+      p: `Notes stayed updated across all your devices, automatically and in real-time. There's no "sync" button: It just works.`,
+    },
+    {
+      h5: "Stay Organized",
+      p: "Add tags to find notes quickly with instant searching.",
+    },
+    {
+      h5: "Work Together",
+      p: "Share a to-do list, post some instructions, or publish your notes online.",
+    },
+    {
+      h5: "Go Back in Time",
+      p: "Notes are backed up with every change, so you can see what you noted last week, or last month.",
+    },
+    {
+      h5: "Markdown Support",
+      p: "Write, preview, and publish your notes in Markdown format.",
+    },
+    {
+      h5: "It's Free",
+      p: "Apps, backups, syncing, sharing - it's all completely free.",
+    },
+  ];
 
   const signUpButtonClass = `border-12 m-auto min-w-50 min-h-20 rounded-4xl font-extrabold text-2xl text-blue-400 bg-white
      hover:text-white hover:bg-blue-400 hover:border-white hover:shadow-xl hover:shadow-blue-400
@@ -35,6 +67,7 @@ export default function MainPage():JSX.Element {
       >
         <Header />
       </motion.div>
+
       <section className="h-screen pt-30 bg-[#393940] grid items-center">
         <h1 className="text-4xl text-center text-[#dddddd] font-bold font-serif ">
           Make Note Sharing Easy with
@@ -59,48 +92,36 @@ export default function MainPage():JSX.Element {
           <Link to="/register">Sign Up Now</Link>
         </button>
       </section>
-      <section>Section for image</section>
-      <section className="h-screen">
-        <h1>Comprehensive underneath, simple on the surface</h1>
-        <ul>
-          <li>
-            <h5>Use it everywhere</h5>
-            <p>
-              Notes stayed updated across all your devices, automatically and in
-              real-time. There&apos;s no &quot;sync&quot; button: It just works.
-            </p>
-          </li>
-          <li>
-            <h5>Stay Organized</h5>
-            <p>Add tags to find notes quickly with instant searching.</p>
-          </li>
-          <li>
-            <h5>Work Together</h5>
-            <p>
-              Share a to-do list, post some instructions, or publish your notes
-              online.
-            </p>
-          </li>
-          <li>
-            <h5>Go Back in Time</h5>
-            <p>
-              Notes are backed up with every change, so you can see what you
-              noted last week, or last month.
-            </p>
-          </li>
-          <li>
-            <h5>Markdown Support</h5>
-            <p>Write, preview, and publish your notes in Markdown format.</p>
-          </li>
-          <li>
-            <h5>It's Free</h5>
-            <p>
-              Apps, backups, syncing, sharing - it&apos;s all completely free.
-            </p>
-          </li>
+
+      <section className="h-[50vh] bg-gray-400 text-4xl text-center " >Section for image</section>
+
+      <section className="h-screen max-w-250 m-auto bg-cyan-700 text-2xl flex flex-col justify-center gap-12 item-center text-center text-yellow-300 font-semibold ">
+        <h1 className="text-5xl text-white" >Comprehensive underneath,<br/> simple on the surface</h1>
+        <ul className="grid h-[70%] md:h-[50%] grid-cols-3 px-6 gap-12" >
+          {
+            featureBox.map((item,ind)=> {
+              return (
+                <motion.li
+                  initial={{ opacity: 0, x:"50%", y:"20%" }}
+                  whileInView={{ opacity:1, x: 0 }}
+                  exit={{x:"50%"}}
+                  transition={{ type: "spring", damping: 500, stiffness: 500 }}
+                  className="md:h-[25%] h-[40%] p-1 text-justify hover:bg-cyan-600 animation items-center duration-600 rounded-t-xl "
+                  key={`list-${ind}`}
+                >
+                  <h5 className="text-white text-2xl text-center ">
+                    {item.h5}
+                  </h5>
+                  <p className="text-yellow-200 text-lg hover:bg-cyan-800 h-[12.5vh] rounded-b-xl px-2 ">
+                    {item.p}
+                  </p>
+                </motion.li>
+              );
+            })
+          }
         </ul>
       </section>
-      <hr />
+
       <section>
         <h1>What people are Saying</h1>
         <ul>
@@ -126,7 +147,7 @@ export default function MainPage():JSX.Element {
           </li>
         </ul>
       </section>
-      <hr />
+      
       <Footer />
     </main>
   );
