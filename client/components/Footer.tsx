5adb48962b536f03926bbaf3e78358e4f426feb9
Yogesh Sahu <ysahu1590@gmail.com>
2026-08-04 12:54:09 +0530

    Refactor Footer component with dynamic links and styling
    Refactored Footer component to use a links array for rendering. Updated footer styling with dark theme and added a privacy notice link.

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,27 +1,52 @@
 import { Link } from 'react-router';
 
 export default function Footer(){
+  type Links ={
+    linkTo: string,
+    name: string
+  }
+  const links: Links[] = [
+    {
+      linkTo: "#contac-us",
+      name: "Contact Us"
+    },
+    {
+      linkTo: "#help",
+      name: "Help"
+    },
+    {
+      linkTo: "/blog",
+      name: "Blog"
+    },
+    {
+      linkTo: "/devs",
+      name: "Developers"
+    },
+    {
+      linkTo: "/tnc",
+      name: "Terms & Conditions"
+    },
+    {
+      linkTo: "/privacy",
+      name: "Privacy"
+    }
+
+  ]
     return (
-      <footer>
-        <ul>
-          <li>
-            <Link to="#contact-us">Contact Us</Link>
-          </li>
-          <li>
-            <Link to="#help">Help</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/devs">Developers</Link>
-          </li>
-          <li>
-            <Link to="/tnc">terms & Conditions</Link>
-          </li>
-          <li>
-            <Link to="/privacy">Privacy</Link>
-          </li>
+      <footer className="rounded-xl font-serif text-xs md:text-lg py-10 mt-5 bg-linear-to-t from-blue-500 to-red-500 dark:bg-linear-to-tr dark:from-purple-500 dark:to-black dark:text-yellow-200 grid items-center justify-center text-center ">
+        <ul className='grid'> 
+          {
+            links.map((item, ind)=> {
+              return(
+                <li className='text-sm hover:text-[#dfdfdf] 'key={`footer-${ind}`} >
+                  <Link to={`${item.linkTo}`}>
+                  {item.name}
+                  </Link>
+                </li>
+              )
+            })
+          }
+          
         </ul>
         <a href="https://www.startupindia.gov.in/content/sih/en/privacy-policy.html">
           Privacy Notice for Indian Users
