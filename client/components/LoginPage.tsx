e27269158999af2d4c5806cc84bb2c7e2670316a
yashukaSan <ysahu1590@gmail.com>
2026-08-08 00:35:16 +0530

    Enhace the interface for mobile screens and update dynamic componenet sizing

--- client/components/LoginPage.tsx
+++ client/components/LoginPage.tsx
@@ -6,12 +6,12 @@ export default function LoginPage(){
     const [ showPass, setShowPass ] = useState(false);
     return (
       <>
-        <section className="h-screen pt-40 items-center dark:bg-[#191919] ">
-          <form className="flex flex-col min-w-120 w-[45%] rounded-3xl gap-5 justify-center items-center border-[#dedede] text-white border-3 m-auto h-[40vh] ">
-            <h1 className="text-3xl bg-linear-to-l py-2 px-10 w-100 text-center rounded-2xl border-3 border-yellow-400 from-blue-300 to-red-400 " >
+        <section className="py-12 overflow-hidden bg-blue-600 min-h-screen flex items-center justify-center dark:bg-[#191919] ">
+          <form className="flex rounded-3xl flex-col gap-5 justify-center items-center border-[#fe2e9e] text-white border-6 min-w-60 h-full p-8">
+            <h1 className=" font-serif md:text-3xl text-xl bg-linear-to-l py-2 px-10 w-full text-center rounded-2xl border-3 border-yellow-400 from-blue-300 to-red-400 ">
               Welcome Back
             </h1>
-            <label className="border-l-red-400 border-t-red-400 border-r-blue-500 border-b-blue-500 border-3 flex p-2 min-w-100 rounded-xl justify-center items-center gap-10 ">
+            <label className="border-l-red-400 border-t-red-400 border-r-blue-500 border-b-blue-500 border-3 grid grid-cols-2 p-2 w-full xl:w-[45%] rounded-xl justify-around items-center 10 ">
               Username:
               <input
                 type="text"
@@ -19,36 +19,34 @@ export default function LoginPage(){
                 className="hover:bg-[#4d4d4d] rounded-xl text-center p-1"
               />
             </label>
-            <label className="flex border-3 border-t-blue-500 border-l-blue-500 border-r-red-400 border-b-red-400 p-2 rounded-xl w-100 justify-center items-center gap-10">
+            <label className="border-l-red-400 border-t-red-400 border-r-blue-500 border-b-blue-500 border-3 p-2 w-full rounded-xl grid grid-cols-2 justify-around items-center gap-4 md:gap-10 ">
               Password:
-              <div className="flex items-center justify-center">
-                <input
-                  type={showPass ? "text" : "password"}
-                  placeholder="e.g., 12345678"
-                  className="hover:bg-[#3d3d3d] text-center rounded-lg p-1 "
-                />
-                <button className="hover:bg-[#dfdfdf] hover:text-black text-[#efdfef] rounded-full">
-                  {showPass ? (
-                    <Eye
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setShowPass(false);
-                      }}
-                    />
-                  ) : (
-                    <EyeClosed
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setShowPass(true);
-                      }}
-                    />
-                  )}
-                </button>
-              </div>
+              <input
+                type={showPass ? "text" : "password"}
+                placeholder="e.g., 12345678"
+                className="hover:bg-[#3d3d3d] text-center rounded-lg p-1 "
+              />
+              <button className="hover:bg-[#dfdfdf] hover:text-black text-[#efdfef] rounded-full">
+                {showPass ? (
+                  <Eye
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setShowPass(false);
+                    }}
+                  />
+                ) : (
+                  <EyeClosed
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setShowPass(true);
+                    }}
+                  />
+                )}
+              </button>
             </label>
             <button
               type="submit"
-              className="border-2 w-100 rounded-xl p-2 hover:p-[2.7px] hover:font-bold hover:text-lg border-t-purple-400 border-l-yellow-400 border-b-yellow-500 border-r-purple-500 hover:bg-linear-to-tr from-purple-700 to-yellow-600 hover:border-4"
+              className="border-2 w-full max-w-lg rounded-xl p-2 hover:p-[2.7px] hover:font-bold hover:text-lg border-t-purple-400 border-l-yellow-400 border-b-yellow-500 border-r-purple-500 hover:bg-linear-to-tr from-purple-700 to-yellow-600 hover:border-4"
             >
               LOGIN
             </button>
