17f576e513f31d841a7b4de6c52f785ced20f4ff
yashukaSan <ysahu1590@gmail.com>
2026-08-15 01:20:48 +0530

    correct the cors error and add the cookieParser method with some imports

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,22 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors'
 import userRouter from "./route/user.route";
+import routerData from "./route/alldata"
+import protect from './route/protect.route';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
 
 import router from './route/auth'
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth', router);
 
 app.get('/', (req: Request, res: Response) => {
