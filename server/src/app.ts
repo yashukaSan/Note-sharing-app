b810f1d4b39f5f99358ab3550124bad223d1442f
yashukaSan <ysahu1590@gmail.com>
2026-08-15 22:42:03 +0530

    remove the unwanted imports

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,9 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors'
 import userRouter from "./route/user.route";
-import routerData from "./route/alldata"
-import protect from './route/protect.route';
-import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 
-
-import router from './route/auth'
+import router from './route/auth';
 
 const app = express();
 
@@ -21,21 +17,6 @@ app.use('/api/auth', router);
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Server is alive and running");
-});
-
-app.post('/api/register', (req: Request, res: Response) => {
-    const { Uname, Uemail, username, password } = req.body;
-
-    console.log('received Registration Data: ', { Uname, Uemail, username });
-
-    if(!Uemail || !password || !username) {
-        return res.status(400).json({ message: "Email, username, and password are required"});
-    }
-
-    return res.status(200).json({
-        message: "Everything is OKAY, registration successful",
-        user: {Uname, Uemail, username}
-    });
 });
 
 //router declaration 
