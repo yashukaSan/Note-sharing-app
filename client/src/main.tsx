011000c8bda9f2df1db5e270106daf726ce26c52
yashukaSan <ysahu1590@gmail.com>
2026-08-17 23:56:17 +0530

    make the multiple  routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,33 @@
 import { StrictMode } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router';
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx';
-import LoginPage from '../components/LoginPage.tsx';
-import RegisterUser from "../components/RegisterUser.tsx";
+import App from "./App.tsx";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+import ProtectedRoute from "../components/protectedMain";
+import MainPage from "../components/LandingPage";
+import "./App.css";
+import LoginPage from "../components/LoginPage";
+import RegisterPage from "../components/RegisterUser";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />
-  },
-  {
-    path: "/register",
-    element: <RegisterUser />
-  }
-])
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/success"
+          element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="#" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 );
