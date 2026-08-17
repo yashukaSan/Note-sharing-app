5071c88e213ebe8a95590fa8bc490fcbf3914a71
yashukaSan <ysahu1590@gmail.com>
2026-08-17 23:56:54 +0530

    restructure ethe file and put only the required data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
-import ProtectedRoute from '../components/protectedMain';
-import MainPage from '../components/LandingPage';
 import './App.css'
 import LandingPage from '../components/LandingPage';
-import LoginPage from '../components/LoginPage';
-import RegisterPage from '../components/RegisterUser'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LandingPage/>} />
-        <Route path='login' element={<LoginPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
-
-        <Route path='/success' element={<ProtectedRoute><MainPage/></ProtectedRoute>} />
-
-        <Route path="#" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-
-    // <>
-    //   <section className="h-screen w-screen bg-linear-to-tr from-blue-600 via-red-500 to-cyan-300 text-red-400" >
-    //     <MainPage />
-        // {/* <LoginPage /> */}
-        // {/* <RegisterUser/> */}
-    //   </section>
-    // </>
-  )
+  return <LandingPage />;
 }
 
 export default App
