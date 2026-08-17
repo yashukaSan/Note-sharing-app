106d6a72ce9391c13b24af31d3ce0606322f6dba
yashukaSan <ysahu1590@gmail.com>
2026-08-17 23:41:50 +0530

    update the routing system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,34 @@
-// import { useState } from 'react'
-import LoginPage from "../components/LoginPage";
-import RegisterUser from "../components/RegisterUser";
-import MainPage from '../components/MainPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
+import ProtectedRoute from '../components/protectedMain';
+import MainPage from '../components/LandingPage';
 import './App.css'
+import LandingPage from '../components/LandingPage';
+import LoginPage from '../components/LoginPage';
+import RegisterPage from '../components/RegisterUser'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section>
-        <MainPage />
-        {/* <LoginPage /> */}
-        {/* <RegisterUser/> */}
-      </section>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+        <Route path='login' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+
+        <Route path='/success' element={<ProtectedRoute><MainPage/></ProtectedRoute>} />
+
+        <Route path="#" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+
+    // <>
+    //   <section className="h-screen w-screen bg-linear-to-tr from-blue-600 via-red-500 to-cyan-300 text-red-400" >
+    //     <MainPage />
+        // {/* <LoginPage /> */}
+        // {/* <RegisterUser/> */}
+    //   </section>
+    // </>
   )
 }
 
