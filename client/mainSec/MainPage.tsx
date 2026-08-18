86d917b5bcbb576335dfa1cfd18785411d4636b2
yashukaSan <ysahu1590@gmail.com>
2026-08-19 00:34:46 +0530

    added the basic layout

--- client/mainSec/MainPage.tsx
+++ client/mainSec/MainPage.tsx
@@ -1,18 +1,23 @@
 import Header from '../components/Header';
+import Footer from '../components/Footer'
 
 function MainPage(){
   const name:string = localStorage.getItem("name")!;
   
     return (
-      <>
-        <Header />
+      <main className="flex justify-between flex-col h-full min-h-screen">
+        <section>
+          <Header />
 
-        <section className=" h-20 border bg-linear-to-tr from-red-600 to-black " > 
-          <h2 className=" lg:font-bold lg:text-3xl text-xl font-semibold pt-4 text-yellow-300 text-center " >
-            Welcome Back, {name.charAt(0).toUpperCase() + name.slice(1)}
-          </h2>
+          <section className=" h-20 border bg-linear-to-tr  from-red-600 to-black ">
+            <h2 className=" lg:font-bold lg:text-3xl text-xl font-semibold pt-4 text-yellow-300 text-center ">
+              Welcome Back, {name.charAt(0).toUpperCase() + name.slice(1)}
+            </h2>
+          </section>
         </section>
-      </>
+
+        <Footer />
+      </main>
     );
 }
 
